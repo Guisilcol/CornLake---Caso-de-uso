ec7f193cf4e737180ea1f5ec330bd5f38b60b636
Guilherme S. Magalhaes <silcol455@gmail.com>
2024-01-04 15:35:36 -0300

    Update build.sh script with requirements and instructions

--- lambda-cornlake-01-gerador-de-dados-no-sistema-transacional/build.sh
+++ lambda-cornlake-01-gerador-de-dados-no-sistema-transacional/build.sh
@@ -1,8 +1,9 @@
 # Author: Guilherme dos Santos Magalhães 
 # Date: 2021-02-21
 # Description: Build and deploy the lambda function. 
-# Requirements: SAM CLI, AWS CLI, Poetry
+# Requirements: SAM CLI, AWS CLI, Poetry. Don't forget to configure the template.yaml file with de values of prod environment variables.
 # Usage: ./build.sh
+
 
 RESOURCE_NAME=DataGenerator
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
